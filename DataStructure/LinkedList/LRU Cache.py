78df0fd98eeba32d07e8aa6276b5df3d8804f815
Sarthak Jain <sarthakjain31797@gmail.com>
2020-09-12 12:04:44 -0700

    LRU Added

--- DataStructure/LinkedList/LRU Cache.py
+++ DataStructure/LinkedList/LRU Cache.py
@@ -1,67 +1,61 @@
-#LRU Cache
-class Node:
-    def __init__(self,data):
-        self.prev=None
-        self.next=None
-        self.data=data
+class LruCache(object):
+    def __init__(self,capacity):
+        self.capacity=capacity
+        self.cache=[]
+        self.Lru={}
+        self.tm=0
         
+    def get(self,key):
+        #Get the Data from Cache
+        if key in self.cache:#If Key Exist in Cache
+            self.lru[key]=self.tm
+            self.tm+=1
+            print("Cache = ")
+            return 1
+        else:
+            return -1
+    def set(self,key):
+        if len(self.cache)>self.capacity:
+            if key not in self.Lru: 
+                #Find Least Entry
+                old_Key=min(self.Lru.keys(),key=lambda k:self.Lru[k])#Vary Important Logic
+                #print("Old Value ======",old_Key)
+                #Just Delete Old Value
+                for i in range(len(self.cache)):
+                    if self.cache[i]==old_Key:
+                        self.cache.pop(i)
+                        print("Duplicate",self.cache)
+                        break
+                    
+                self.Lru.pop(old_Key)
 
-class LinkedList:
-    def __init__(self):
-        self.head=None
-        self.limit=3
-        self.current=0
+            
+                self.Lru[key]=self.tm
+                self.cache.append(key)
+                self.tm+=1
+            else:
+                self.Lru[key]=self.tm
+                self.tm+=1
+                
+        else:
 
-   
-    def Delete(self):
-        temp=self.head
-        self.head=self.head.next
-        self.head.next.prev=None
-
-    def PrintData(self):
-        temp=self.head
-        while(temp):
-            print("LRU = ",temp.data)
-            temp=temp.next
+            if key in self.Lru:
+                self.Lru[key]=self.tm
+                self.tm=self.tm+1
+            else:
+                self.cache.append(key)
+                self.Lru[key]=self.tm
+                self.tm+=1
+        #print("LRU: {}".format(self.Lru))
+        print("Cache : {}".format(self.cache))
 
 if __name__=="__main__":
-    n1=LinkedList()
-    n1.head=Node(1)
-    n2=Node(2)
-    n3=Node(3)
-    n4=Node(4)
-    n5=Node(1)
-    n6=Node(2)
-    n7=Node(5)
-    n8=Node(1)
-    n9=Node(2)
-    n10=Node(3)
-    n11=Node(4)
-    n12=Node(5)
-
-    n1.head.next=n2
-    n2.next=n3
-    n3.next=n4
-    n4.next=n5
-    n5.next=n6
-    n6.next=n7
-    n7.next=n8
-    n8.next=n9
-    n9.next=n10
-    n10.next=n11
-    n11.next=n12
-    
-    n12.prev=n11
-    n11.prev=n10
-    n10.prev=n9
-    n9.prev=n8
-    n8.prev=n7
-    n7.prev=n6
-    n6.prev=n5
-    n5.prev=n4
-    n4.prev=n3
-    n3.prev=n2
-    n2.prev=n1
-
-
-    n1.PrintData()
+    l1=LruCache(capacity=3)
+    l1.set(1)
+    l1.set(2)
+    l1.set(3)
+    l1.set(3)
+    l1.set(1)
+    l1.set(5)
+    l1.set(2)
+    l1.set(8)
