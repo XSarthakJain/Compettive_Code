4de8e4c6ced951d54e36b6a511851af58f90aa7d
Sarthak Jain <sarthakjain31797@gmail.com>
2020-09-14 10:59:55 -0700

    Add AddChild and Level Method in Implementation Of Tree

--- DataStructure/Tree/Implementation_Of_Tree.py
+++ DataStructure/Tree/Implementation_Of_Tree.py
@@ -9,10 +9,19 @@ class TreeNode:
         data.parent=self
         self.child.append(data)
     def PrintData(self):
-        print(self.data)
+        space=" "*self.Level()*3
+        print(space + str(self.data))
         if self.child:
             for i in self.child:  
                 i.PrintData()
+
+    def Level(self):
+        levelCounter=0
+        temp=self.parent
+        while(temp):
+            levelCounter+=1
+            temp=temp.parent
+        return levelCounter
                 
 
 t1=TreeNode(5)
@@ -25,5 +34,5 @@ t4=TreeNode(12)
 t1.addChild(t4)
 t5=TreeNode(13)
 t2.addChild(t5)
-        
+
 t1.PrintData()
