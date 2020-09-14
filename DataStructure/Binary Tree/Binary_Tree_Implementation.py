6a140df55c3002a08ae2764e7603f04686f6f133
Sarthak Jain <sarthakjain31797@gmail.com>
2020-09-14 12:10:45 -0700

    Add Inorder,Preorder,Postorder Traversal in Binary Tree

--- DataStructure/Binary Tree/Binary_Tree_Implementation.py
+++ DataStructure/Binary Tree/Binary_Tree_Implementation.py
@@ -1,22 +1,53 @@
 #Binary Tree
-class Node:
+class tree:
     def __init__(self,data):
-        self.data=None
+        self.data=data
         self.left=None
         self.right=None
-class tree:
-    def __init__(self):
-        self.root=None
-    def Insert(self,data):
-        if self.root is None:
-            self.root=Node(data)
+    def addChild(self,data):
+        
+        if self.left is  None:
+            self.left=data
         else:
-            if self.data<data:
-                Insert(self.right,data)
-            else:
-                Insert(self.left,data)
+            self.right=data
+
+    def Inorder(self):
+        #left->Node->right
+        if self.left:
+            self.left.Inorder()
+        print(self.data)
+        if self.right:
+            self.right.Inorder()
+
+    def Preorder(self):
+        print(self.data)
+        if self.left:
+            self.left.Preorder()
+        if self.right:
+            self.right.Preorder()
+    def Postorder(self):
+        if self.left:
+            self.left.Postorder()
+        if self.right:
+            self.right.Postorder()
+        print(self.data)
+    
         
 
-t1=tree()
-t1.Insert(10)
-t1.Insert(20)
+t1=tree(5)
+t2=tree(10)
+t3=tree(15)
+t4=tree(20)
+t5=tree(25)
+t6=tree(30)
+t1.addChild(t2)
+t1.addChild(t3)
+t2.addChild(t4)
+t2.addChild(t5)
+t4.addChild(t6)
+print("Inorder")
+t1.Inorder()
+print("preorder")
+t1.Preorder()
+print("Postorder")
+t1.Postorder()
