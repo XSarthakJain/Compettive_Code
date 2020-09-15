7d5fcf6016b04d03614bc28a01f72f8d6bb948da
Sarthak Jain <sarthakjain31797@gmail.com>
2020-09-15 14:16:02 -0700

    Added Deletion in Implementation Of BST

--- DataStructure/BST/Implementation_OF_Binary_Serach_Tree.py
+++ DataStructure/BST/Implementation_OF_Binary_Serach_Tree.py
@@ -9,7 +9,7 @@ class BST:
         if self.data==data:
             return
         
-        if data>int(self.data):
+        if data>self.data:
             if self.right:
                 self.right.addChild(data)
             else:
@@ -26,6 +26,61 @@ class BST:
         if self.right:
             self.right.Preorder()
 
+    def Search(self,data):
+        #print("self.data",self.data,data)
+        if self.data==data:
+            print("True")
+            return True
+
+        if data<self.data:
+            if self.left:
+                self.left.Search(data)
+            else:
+                print("False")
+                return False
+        else:
+            if self.right:
+                self.right.Search(data)
+            else:
+                print("False")
+                return False
+    def MinValueTree(self):
+        current=self
+        
+        while(current.left is not None):
+            current=current.left
+        return current
+
+    def Deletion(root,key):
+        print("yghyuhjguyg",root.data)
+        if root is None:
+            return root
+        
+        elif key>root.data:
+            root.right=root.right.Deletion(root.right,key)
+        elif key<root.data:
+            root.left=root.left.Deletion(root.left,key)
+        else:
+            if root.left is None:
+                temp=root.right
+                root=None
+                return temp
+            elif root.right is None:
+                temp=root.left
+                root=None
+                return temp
+            else:
+                temp=root.right.MinValueTree()
+                print("temp.data",temp.data)
+                root.data=temp.data
+                root.right=root.right.Deletion(temp.data)
+
+        return root
+            
+            
+            
+        
+
 def Build(data1):
     root=BST(data1[0])
 
@@ -38,5 +93,8 @@ if __name__=="__main__":
     l1=[5,10,15,25,2]
     n1=Build(l1)
     n1.Preorder()
-    
+    n1.Search(2)
+
+    n1=n1.Deletion(5)
+    n1.Preorder()
             
