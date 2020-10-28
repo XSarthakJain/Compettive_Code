a64018e8f6f7d25cace6b08ba4a57001621642a1
Sarthak Jain <sarthakjain31797@gmail.com>
2020-10-28 18:16:33 +0530

    Implement Chocklate Distribution Problem

---
+++ LoveBubber/Chocklate Distribution Problem.py
@@ -0,0 +1,15 @@
+#Chocklate Distribution Problem
+#code
+for _ in range(int(input())):
+    N=int(input())
+    A=list(map(int,input().split()))
+    M=int(input())
+    A.sort()
+    result=A[M-1]-A[0]
+    current=0
+    for i in range(N):
+        if i>=M-1:
+            
+            result=min(result,A[i]-A[current])
+            current+=1
+    print(result)
