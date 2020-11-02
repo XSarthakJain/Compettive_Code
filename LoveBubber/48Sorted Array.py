5415f4a50c3a2932bca75d89a673ef78d91208e4
Sarthak Jain <sarthakjain31797@gmail.com>
2020-11-02 19:26:14 +0530

    Implement Sorted Array Question Added

---
+++ LoveBubber/48Sorted Array.py
@@ -0,0 +1,11 @@
+#Sorted Array
+#code
+
+for _ in range(int(input())):
+    n=int(input())
+    a=list(map(int,input().split()))
+    for i in range(len(a)):
+        for j in range(i+1,len(a)):
+            if a[i]>a[j]:
+                a[i],a[j]=a[j],a[i]
+print(*a)
