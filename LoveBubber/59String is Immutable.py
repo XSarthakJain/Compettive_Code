9f051df113d606e286bfdef373ccfcd1de642039
Sarthak Jain <sarthakjain31797@gmail.com>
2020-11-04 23:47:00 +0530

    Add String is Immutable

---
+++ LoveBubber/59String is Immutable.py
@@ -0,0 +1,5 @@
+#String is Immutable https://youtu.be/to9DPVsdByE
+Why string is immutable with example?
+
+In the String constant pool, a String object is likely to have one or many references. If several references point to same String without even knowing it,
+it would be bad if one of the references modified that String value. That's why String objects are immutable.
