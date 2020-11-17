f8db1456ba6f80f8a6b9dbb279aa772c925e0eed
Sarthak Jain <sarthakjain31797@gmail.com>
2020-11-17 23:40:46 +0530

    Complete Implementation of Longest Palindrome

--- LoveBubber/63Longest Palindrome in a String.py
+++ LoveBubber/63Longest Palindrome in a String.py
@@ -1,11 +1,13 @@
 for _ in range(int(input())):
     S=input()
     l=S[0]
+    
     for i in range(len(S)):
        for j in range(len(S)-1,i,-1):
            #print(S[i],S[j])
            if S[i]==S[j]:
+               temp=S[i:j+1]
                #print(S[i:j+1],S[j:i-1:-1])
-               if S[i:j+1]==S[j:i-1:-1]: 
-                   l=max(S[i:j+1],l,key=len)
+               if temp==temp[::-1]: 
+                   l=max(l,S[i:j+1],key=len)
     print(l)
