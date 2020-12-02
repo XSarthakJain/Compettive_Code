9a7c9f8e44679f06c68a6cdfb21512e3220550e6
Sarthak Jain <sarthakjain31797@gmail.com>
2020-12-03 01:43:09 +0530

    Implement all Value Seperated by * by Recursion

---
+++ Recursion/allStar.py
@@ -0,0 +1,18 @@
+#allStar
+'''
+Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+
+
+allStar("hello") → "h*e*l*l*o"
+allStar("abc") → "a*b*c"
+allStar("ab") → "a*b"
+'''
+
+def allStar(s):
+    if len(s)==1:
+        return s[-1]
+    else:
+        return s[0]+"*"+allStar(s[1:len(s)])
+
+s=input()
+print(allStar(s))
