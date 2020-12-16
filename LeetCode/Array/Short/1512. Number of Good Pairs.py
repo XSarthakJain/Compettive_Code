fa03240859d6d7083673b9af76146103375307de
Sarthak Jain <sarthakjain31797@gmail.com>
2020-12-16 23:24:29 +0530

    Implement Number of Good Pairs

---
+++ LeetCode/Array/Short/1512. Number of Good Pairs.py
@@ -0,0 +1,13 @@
+#1512. Number of Good Pairs
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result=0
+        for i in range(len(nums)):
+            for j in range((i+1),len(nums)):
+                if nums[i]==nums[j]:
+                    result+=1
+        return result
