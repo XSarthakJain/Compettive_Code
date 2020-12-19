93aa1dd2d7fcb7d399119bea1f47416c540b6dff
Sarthak Jain <sarthakjain31797@gmail.com>
2020-12-19 23:46:12 +0530

    Modify 1534. Count Good Triplets

--- LeetCode/Array/Short/1534. Count Good Triplets.py
+++ LeetCode/Array/Short/1534. Count Good Triplets.py
@@ -1,1 +1,12 @@
 #1534. Count Good Triplets
+arr=list(map(int,input().split()))
+a,b,c=map(int,input().split())
+count=0
+for i in range(len(arr)-2):
+    for j in range(i+1,len(arr)-1):
+        for k in range(j+1,len(arr)):
+            if (abs(arr[i]-arr[j])<=a) and (abs(arr[j]-arr[k])<=b) and (abs(arr[i]-arr[k])<=c):
+                print("i,j,k",arr[i],arr[j],arr[k])
+                count+=1
+print(count)
+     
