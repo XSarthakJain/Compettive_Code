45d803667c32d530ec7df4a888582a657298fc6e
Sarthak Jain <sarthakjain31797@gmail.com>
2020-12-23 00:14:55 +0530

    Modify 1252. Cells with Odd values in a Matrix

--- LeetCode/Array/Short/1252. Cells with Odd Values in a Matrix.py
+++ LeetCode/Array/Short/1252. Cells with Odd Values in a Matrix.py
@@ -1,1 +1,32 @@
 #1252. Cells with Odd Values in a Matrix
+
+n = 2
+m = 2
+indices = [[1,1],[0,0]]
+#array=[[0]*m]*n
+array=[]
+
+
+for i in range(n):
+    temp=[]
+    for j in range(m):
+        temp.append(0)
+    array.append(temp)
+        
+for i in indices:
+    row_item=i[0]
+    col_item=i[1]
+    
+    for col in range(m):
+        array[row_item][col]+=1
+
+    for row in range(n):
+        array[row][col_item]+=1
+
+   
+result=0
+for i in range(n):
+    for j in range(m):
+        if array[i][j] & 1:
+            result+=1
+print(result)
