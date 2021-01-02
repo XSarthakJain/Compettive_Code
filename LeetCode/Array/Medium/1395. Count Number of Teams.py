8bbc23c33195e7768d5a90c9d8fe60a43b25b00f
Sarthak Jain <sarthakjain31797@gmail.com>
2021-01-02 22:23:27 +0530

    Implement 1395 Count Number of Terms

---
+++ LeetCode/Array/Medium/1395. Count Number of Teams.py
@@ -0,0 +1,14 @@
+#1395. Count Number of Teams
+class Solution(object):
+    def numTeams(self, rating):
+        """
+        :type rating: List[int]
+        :rtype: int
+        """
+        rate=0
+        for i in range(len(rating)):
+            for j in range(i+1,len(rating)):
+                for k in range(j+1,len(rating)):
+                    if (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]):
+                        rate+=1
+        return rate
