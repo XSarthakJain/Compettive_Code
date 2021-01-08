fac74f0afbf2c6c4228f8a910d2f88e30dc40985
Sarthak Jain <sarthakjain31797@gmail.com>
2021-01-09 01:26:14 +0530

    Implement 1414. Find the Minimum Number of Fibonacci Numbers Whose Sum Is K

--- LeetCode/Array/Medium/1414. Find the Minimum Number of Fibonacci Numbers Whose Sum Is K.py
+++ LeetCode/Array/Medium/1414. Find the Minimum Number of Fibonacci Numbers Whose Sum Is K.py
@@ -1,6 +1,24 @@
 #1414. Find the Minimum Number of Fibonacci Numbers Whose Sum Is K
 n=int(input())
-a=1
-b=1
 
-result=[]
+
+result = []
+def fibo(n1):
+    a=1
+    b=1
+    c=1
+    while(True):
+        a=b
+        b=c
+        c=a+b
+        if a>n1:
+            return (b-a)
+            break
+c=0
+while(True):
+    n = n-fibo(n)
+    c+=1
+    if n == 0:
+        break
+print(c)
+    
