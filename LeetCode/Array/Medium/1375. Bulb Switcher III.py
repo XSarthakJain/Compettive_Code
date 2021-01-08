666a6d4f99b294a6b42f4dc27aecc4e0b1eeda9f
Sarthak Jain <sarthakjain31797@gmail.com>
2021-01-09 00:52:29 +0530

    Modify Corner case and its time Complexity

--- LeetCode/Array/Medium/1375. Bulb Switcher III.py
+++ LeetCode/Array/Medium/1375. Bulb Switcher III.py
@@ -1,31 +1,17 @@
 #1375. Bulb Switcher III
-light = [2,1,3,5,4]
-result=[]
-c=0
-l=len(light)
-sum_list = (l*(l+1))//2
+light = [2,1,4,3,6,5]
+result = []
+c = 0
+last = 0
+val=0
 for i in range(len(light)):
     result.append(0)
 for i in light:
-    result[i-1] = i
-    flag = True
-    for j in range(i,0,-1):
-        if j not in result:
-            flag = False
-            break
-    if flag == True:
-        print("len(result)",len(result))
-        print("Check",result.count(0),(len(result)-i),result,i)
-        if result.count(0)==(len(result)-i-1):
-            c+=1
-        '''
-        for k in range(len(result)-1,-1,-1):
-            print(
-            if sum(result[k:-1]) == (((k+1)*(k+2))//2):
-                c+=1
-                break'''
-        
-        
-print(result,c)
-        
-    
+    val+=1
+    result[i-1] = 1
+    flag = False
+    #Left Blue Bulb
+    last = max(last,i)
+    if last == val:
+        c+=1
+print(c)
