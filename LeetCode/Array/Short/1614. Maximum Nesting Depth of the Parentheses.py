c2ce30a01550f7292b117c3b6be9c6694140a0ed
Sarthak Jain <sarthakjain31797@gmail.com>
2021-01-10 23:00:02 +0530

    Implement 1614 Program of 1614 Maximum Nesting Depth

--- LeetCode/Array/Short/1614. Maximum Nesting Depth of the Parentheses.py
+++ LeetCode/Array/Short/1614. Maximum Nesting Depth of the Parentheses.py
@@ -1,3 +1,13 @@
 #1614. Maximum Nesting Depth of the Parentheses
-s = "(1+(2*3)+((8)/4))+1"
+s =  "1"
 left=[]
+c=0
+for i in s:
+    if i == "(":
+        left.append(i)
+        c = max(c,len(left))
+    elif i == ")":
+        left.pop()
+
+print(c)
+        
