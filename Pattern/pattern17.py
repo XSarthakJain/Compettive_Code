c32c2f1866400acb3079c0300d2ce2c95e86a1cb
Sarthak Jain <sarthakjain31797@gmail.com>
2022-01-01 23:59:26 +0530

    Implement Pattern17

---
+++ Pattern/pattern17.py
@@ -0,0 +1,13 @@
+nRow = 4
+nCol =4
+
+for row in range(nRow,0,-1):
+    '''
+    Python Solution
+    print("*"*row,end="\n")
+    '''
+
+    #All Language Support
+    for col in range(row,0,-1):
+        print("*",end="")
+    print("")
