fcf96a69c3051da19bca253c37fb68eef9f32cbf
Sarthak Jain <sarthakjain31797@gmail.com>
2022-12-14 17:21:14 +0530

    Solved Island Perimeter

---
+++ LeetCode/463. Island Perimeter.py
@@ -0,0 +1,17 @@
+#463. Island Perimeter
+grid = [[1]]
+result = 0
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        if grid[i][j] == 1:
+            if j==0 or grid[i][j-1] == 0:
+                result+=1
+            if j==len(grid[0])-1 or grid[i][j+1] == 0:
+                result+=1
+            if i==0 or grid[i-1][j] == 0:
+                result+=1
+            if i==len(grid)-1 or grid[i+1][j] == 0:
+                result+=1
+print(result)
+            
+                
