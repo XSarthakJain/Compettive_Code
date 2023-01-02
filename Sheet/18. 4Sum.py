8738924f81f322eea189795f8fac2c867261ea23
Sarthak Jain <sarthakjain31797@gmail.com>
2023-01-02 23:56:40 +0530

    Added 4 Sum Optimal Force Solution

--- Sheet/18. 4Sum.py
+++ Sheet/18. 4Sum.py
@@ -3,6 +3,36 @@ nums = [1,0,-1,0,-2,2]
 target = 0
 nums.sort()
 result = []
+
+#Optimal Solution
+for i in range(0,len(nums)-2):
+    for j in range(i+1,len(nums)-1):
+        left = j+1
+        right = len(nums)-1
+        desire_ele = target - nums[i]-nums[j]
+        while(left<right):
+            if nums[left]+nums[right] == desire_ele:
+                temp = [nums[i],nums[j],nums[left],nums[right]]
+                print([nums[i],nums[j],nums[left],nums[right]])
+                temp.sort()
+                print(temp)
+                if temp not in result:
+                    result.append(temp)
+                temp_left = nums[left]
+                temp_right = nums[right]
+                while(left<right and nums[left]==temp_left):
+                    left+=1
+                while(left<right and nums[right]==temp_right):
+                    right-=1
+            elif nums[left]+nums[right] > desire_ele:
+                right-=1
+            else:
+                left+=1
+                
+        
+
+#Brute Force Approach
+'''
 for i in range(0,len(nums)-3):
     for j in range(i+1,len(nums)-2):
         for k in range(j+1,len(nums)-1):
@@ -23,7 +53,7 @@ for i in range(0,len(nums)-3):
                 else:
                     left = mid+1
 
-                
+'''              
 print("Second Element = ",result)
     
 
