2a32f9ade261ddfd15848f2498a6f8b684623a2d
Sarthak Jain <sarthakjain31797@gmail.com>
2023-01-03 17:20:46 +0530

    Add Longest Consecutive Sequence Optimal Solution

--- Sheet/128. Longest Consecutive Sequence.py
+++ Sheet/128. Longest Consecutive Sequence.py
@@ -1,6 +1,21 @@
 #128. Longest Consecutive Sequence
 nums = [100,4,200,1,3,2]
 result = 0
+
+#Optimal Solution
+data = set(nums)
+for i in range(len(nums)):
+    if nums[i]-1 not in data:
+        temp_result = 0
+        temp=nums[i]
+        while(temp in data):
+            temp+=1
+            temp_result+=1
+        result = max(result,temp_result)
+print(result)
+
+#Brute Force Approach
+'''
 nums.sort()
 if len(nums)==0:
     print(0)
@@ -17,3 +32,4 @@ for i in range(1,len(nums)):
     pivot = nums[i]
 result = max(result,temp_result)
 print(result)
+'''
