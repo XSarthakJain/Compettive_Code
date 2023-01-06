ece8071e3967f6356fb420382868d343519bf36f
Sarthak Jain <sarthakjain31797@gmail.com>
2023-01-06 22:47:12 +0530

    944 Delete Column to make Sorted

--- LeetCode/944. Delete Columns to Make Sorted.py
+++ LeetCode/944. Delete Columns to Make Sorted.py
@@ -2,9 +2,19 @@
 strs =  ["cba","daf","ghi"]
 result = 0
 for i in range(len(strs[0])):
+    prev = strs[0][i]
+    for j in range(len(strs)):
+        if strs[j][i] < prev:
+            result+=1
+            break
+        prev = strs[j][i]
+print(result)
+'''
+for i in range(len(strs[0])):
     temp = []
     for j in range(len(strs)):
         temp.append(strs[j][i])
     if temp != sorted(temp):
         result+=1
 print("Result = ",result)
+'''
