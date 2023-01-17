98332594c04ff2a4120662536ae8d66063aff44a
Sarthak Jain <sarthakjain31797@gmail.com>
2023-01-17 15:41:57 +0530

    Final done 1232 Check If Line is Straight or not

--- LeetCode/1232. Check If It Is a Straight Line.py
+++ LeetCode/1232. Check If It Is a Straight Line.py
@@ -1,23 +1,13 @@
 #1232. Check If It Is a Straight Line
 coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
-coordinates.sort(key=lambda x: (x[0], x[1]))
-first = coordinates[-1][0] - coordinates[0][0]
-second = coordinates[-1][1] - coordinates[0][1]
-prev = coordinates[0]
-for i in range(1,len(coordinates)):
-    print(prev,coordinates[i])
-    if first>0:
-        coordinates[i][0] == prev[0]+1
-    elif first<0:
-        coordinates[i][0] == prev[0]-1
-    else:
-        print(False)
+def calculate(p1,p2):
+    if p1[0] == p2[0]:#Prevent for divide by Zero 
+        return 9999999999999
+    result = (p2[1] - p1[1])/(p2[0]-p1[0])
+    return result
+slop = calculate(coordinates[0],coordinates[1])
+for i in range(2,len(coordinates)):
+    if slop!= calculate(coordinates[i-1],coordinates[i]):
+        return False
+return True
 
-    if second>0:
-        coordinates[i][1] == prev[1]+1
-    elif second<0:
-        coordinates[i][1] == prev[1]-1
-    else:
-        print(False)
-    prev = coordinates[i]
-print(True)
