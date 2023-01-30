54bba05a63cc1437b6442f284e153da6fc22bf69
Sarthak Jain <sarthakjain31797@gmail.com>
2023-01-30 16:32:18 +0530

    Implement LRU by LinkedList and OrderMap Approach

--- LeetCode/146. LRU Cache.py
+++ LeetCode/146. LRU Cache.py
@@ -1,5 +1,55 @@
 #146. LRU Cache
 
+#Approach 1
+class Node:
+    def __init__(self,data,value):
+        self.data = data
+        self.value = value
+        self.next = None
+        self.prev = None
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.data = dict()
+        self.capacity = capacity
+        self.headNode = Node(0,0)
+        self.tailNode = Node(0,0)
+        self.headNode.next = self.tailNode
+        self.tailNode.prev = self.headNode
+    def get(self, key: int) -> int:
+        if key in self.data:
+            self.put(key,self.data[key].value)
+            return self.data[key].value
+        return -1
+    def put(self, key: int, value: int) -> None:
+        if key in self.data:
+            self.data[key].prev.next = self.data[key].next
+            self.data[key].next.prev = self.data[key].prev
+            del self.data[key]
+        elif len(self.data) >= self.capacity:
+            dummykey = self.tailNode.prev.data
+            self.tailNode.prev.prev.next = self.tailNode
+            self.tailNode.prev = self.tailNode.prev.prev
+            del self.data[dummykey]
+        newNode = Node(key,value)
+        newNode.prev = self.headNode
+        newNode.next = self.headNode.next
+        self.headNode.next = newNode
+        newNode.next.prev = newNode
+        self.data[key] = newNode
+obj = LRUCache(2)
+print(obj.put(1,1))
+print(obj.put(2,2))
+print(obj.get(1))
+print(obj.put(3,3))
+print(obj.get(2))
+print(obj.put(4,4))
+print(obj.get(1))
+print(obj.get(3))
+print(obj.get(4))
+
+
+#Approach 2    
+'''
 # LRU BY Ordered Dict
 class LRUCache:
 
@@ -21,3 +71,4 @@ class LRUCache:
         else:
             self.values.pop(key)
         self.values[key] = value
+'''
